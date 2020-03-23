c085573b43c5b3fd30f232e04f0c1a385df66a9a
Itai Gramse <ithai.g@gmail.com>
2020-03-23 11:35:45 +0100

    add method collectBirthDayInfo
    taking a birthday info as a format to query

--- src/ManageBeing.java
+++ src/ManageBeing.java
@@ -19,7 +19,7 @@ public class ManageBeing
         System.out.printf("%-5s%4s\n","ID","Name");
         System.out.println("___________________");
         try{
-            while(rs.next()){ //running throw the resultSet and print it out
+            while(rs.next()){ //iterating throw the resultSet and print it out
                 System.out.printf("%-5s%8s\n",(rs.getString(tableName+"_id")),
                         (rs.getString("first_name") + " " +
                         rs.getString("last_name")));
@@ -28,17 +28,7 @@ public class ManageBeing
             e.printStackTrace();
         }
     }
-    // collecting the information from the user and return a query for the DB.
-    public static String createQueryForAddBeing()
-    {
-        System.out.println("which being would you like to add:\n[1] Employee\n[2] Parent \n[3] Child");
-        int userChoice = ScannerReader.scannerInt(1,3);
-        System.out.println("please type first name");
-        String firstName = ScannerReader.scannerWords();
-        System.out.println("Please type last name");
-        String lastName = ScannerReader.scannerWords();
-        System.out.println("Please type cpr number");
-        String cpr = ScannerReader.scannerIntAsString(10);
+    public static String collectBirthDayInfo(){
         System.out.println("Please type the day of birth");
         String dayBirth = ScannerReader.scannerInt(1,31) +"";
         //fitting the date format
@@ -50,14 +40,30 @@ public class ManageBeing
         String monthBirth = ScannerReader.scannerInt(1,12) + "";
         //fitting the date format
         if (monthBirth.length()<2)  {
-             monthBirth = "0"+ monthBirth;
+            monthBirth = "0"+ monthBirth;
         }
         System.out.println("Please type the year of birth");
         int yearBirth = ScannerReader.scannerInt(1900,9999);
-        String birthDate = yearBirth + "-" + monthBirth +"-"+ dayBirth;
+        return yearBirth + "-" + monthBirth +"-"+ dayBirth;
+
+    }
+    // collecting the information from the user and return a query for the DB.
+    public static String createQueryForAddBeing()
+    {
+        //collecting requirement for any kind of human being
+        System.out.println("which being would you like to add:\n[1] Employee\n[2] Parent \n[3] Child");
+        int userChoice = ScannerReader.scannerInt(1,3);
+        System.out.println("please type first name");
+        String firstName = ScannerReader.scannerWords();
+        System.out.println("Please type last name");
+        String lastName = ScannerReader.scannerWords();
+        System.out.println("Please type cpr number");
+        String cpr = ScannerReader.scannerIntAsString(10);
+        String birthDate = collectBirthDayInfo();
         String tableToUpdate;
         //start to create the values part of the query
         String query = "VALUES ('"+firstName+"', '"+lastName+"', '"+ cpr +"', '"+birthDate +"', '";
+        // collecting the rest of the requirement for specific table
         switch (userChoice){
             case 1:
                 System.out.println("Please type amount of the works hours");
@@ -67,6 +73,7 @@ public class ManageBeing
                 System.out.println("Please type the bank account number");
                 String bankAccount = ScannerReader.scannerIntAsString();
                 tableToUpdate = "employee";
+                //complete the query for employee
                 query = "INSERT INTO "+tableToUpdate+ " (first_name, last_name, cpr, birth_date, work_hours, bank_account)" +
                         query + hoursOfWork +"', '" +salaryPerHour+"', '"+bankAccount +"');"; //complete the query for employee
                 break;
@@ -74,6 +81,7 @@ public class ManageBeing
                 System.out.println("Please type e-mail address");
                 String email = ScannerReader.scannerEMail();
                 tableToUpdate = "parent";
+                //complete the query for parent
                 query = "INSERT INTO" + tableToUpdate +  " (first_name, last_name, cpr, birth_date, email)" +
                         query +email + "');";
                 break;
@@ -81,13 +89,13 @@ public class ManageBeing
                 System.out.println("is the child on the waiting list? \n[0] for not \n[1] for yes)");
                 int isWaiting = ScannerReader.scannerInt(0,1);
                 tableToUpdate = "child";
+                //complete the query for child
                 query = "INSERT INTO "+tableToUpdate+ " (first_name, last_name, cpr, birth_date, is_waiting)"
                         + query + isWaiting + "')";
                 break;
             default:
                 break;
         }
-        System.out.println(query);
         return query;
     }
 

513b97adfe1435f67341fb8fd022ba8b78f78bd0
Itai Gramse <ithai.g@gmail.com>
2020-03-26 13:29:31 +0100

    methods

--- src/ScannerReader.java
+++ src/ScannerReader.java
@@ -1,64 +1,67 @@
 /*this code is taking care about user typing down here you can find different methods for  user console interaction
 this code created by itai gramse
  */
+
 import java.util.*;
 
 public class ScannerReader {
-    public static String scannerAll(){
+    public static String scannerAll ()
+    {
         Scanner console = new Scanner(System.in);
         return console.next();
     }
-    public static String scannerIntAsString()
+
+    public static String scannerIntAsString ()
     {
         Scanner console = new Scanner(System.in);
         String keystroke = console.next();
 
         while (keystroke.matches("^[a-zA-Z]*$")) {
             System.out.println("This doesn't look right, try again only using numbers :)");
-            keystroke=console.next();
+            keystroke = console.next();
         }
 
         return keystroke;
     }
 
-    public static String scannerEMail()
+    public static String scannerEMail ()
     {
         Scanner console = new Scanner(System.in);
         String keystroke = console.next();
-        while (!keystroke.matches("^(.+)@(.+)$"))
-        {
+        while (!keystroke.matches("^(.+)@(.+)$")) {
             System.out.println("This doesn't look right, try again to write the email address :)");
         }
         return keystroke;
     }
 
-    public static String scannerIntAsString(int size)
+    public static String scannerIntAsString (int size)
     {
         Scanner console = new Scanner(System.in);
         String keystroke = console.next();
 
-        while ((keystroke.matches("^[a-zA-Z]*$"))|| (keystroke.length()!=size)) {
+        while ((keystroke.matches("^[a-zA-Z]*$")) || (keystroke.length() != size)) {
             System.out.println("This doesn't look right, try again only using numbers :)");
-            keystroke=console.next();
+            keystroke = console.next();
         }
 
         return keystroke;
     }
 
-    public static String scannerWords()
-    { //Used when needing String input
+    //Used when needing String input
+    public static String scannerWords ()
+    {
         Scanner console = new Scanner(System.in);
         String keystroke = console.next();
 
         while (!keystroke.matches("^[a-zA-Z]*$")) {
             System.out.println("This doesn't look right, try again only using letters :)");
-            keystroke=console.next();
+            keystroke = console.next();
         }
 
         return keystroke;
     }
 
-    public static double scannerDouble()
+    public static double scannerDouble ()
     {
         Scanner console = new Scanner(System.in);
         double num = 0.0;
@@ -68,39 +71,36 @@ public class ScannerReader {
             try {
                 num = console.nextDouble();
                 valid = false;
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 console.next();
                 console.nextLine();
                 System.out.println("You did not enter a number");
             }
-        } while(valid);
+        } while (valid);
 
         return num;
     }
 
-    public static int scannerInt(int min, int max)
+    public static int scannerInt (int min, int max)
     {
         Scanner console = new Scanner(System.in);
         int num = 0;
-        boolean valid = true;
-
         do {
             try {
                 num = console.nextInt();
-                valid = false;
-            }
-            catch(Exception e) {
+                if ( num >= min && num <= max){
+                    return num;
+                }
+                System.out.println("your type is out of range (minimum:"+ min+ " / maximum:"+max+" )");
+            } catch (Exception e) {
                 console.next();
                 console.nextLine();
-                System.out.println("You did not enter a number");
+                System.out.println("You did not enter a number ");
             }
-        } while(valid || num<min || num>max);
-
-        return num;
+        } while (true);
     }
 
-    public static int scannerInt()
+    public static int scannerInt ()
     { //Used when needing int input
         Scanner console = new Scanner(System.in);
         int num = 0;
@@ -110,30 +110,26 @@ public class ScannerReader {
             try {
                 num = console.nextInt();
                 valid = false;
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 console.next();
                 console.nextLine();
                 System.out.println("You did not enter a number");
             }
-        } while(valid);
+        } while (valid);
 
         return num;
     }
 
-
-
     //Used to make number into boolean
-    public static boolean scannerBoolean(int number)
+    public static boolean scannerBoolean (int number)
     {
-        while (number>2 || number<1){
+        while (number > 2 || number < 1) {
             System.out.println("Please type 1 for YES / Type 2 for NO ");
-            number=scannerInt();
+            number = scannerInt();
         }
-        if (number==1){
+        if (number == 1) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
